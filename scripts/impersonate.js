f12989b783c35aaa96e8356faa8f79f37754d6d6
Gary DuVall <duvallg@op9.com>
2015-04-06 22:35:56 -0500

    test

--- scripts/impersonate.js
+++ scripts/impersonate.js
@@ -38,116 +38,114 @@ var shouldTrain = _.constant(_.contains(['train', 'train_respond'], MODE));
 var shouldRespondMode = _.constant(_.contains(['respond', 'train_respond'], MODE));
 
 function robotStore(robot, userId, data) {
-  return robot.brain.set('impersonateMarkov-' + userId, msgpack.pack(data.export()));
+    return robot.brain.set('impersonateMarkov-' + userId, msgpack.pack(data.export()));
 }
 
 function robotRetrieve(robot, cache, userId) {
-  if (cache[userId]) {
-    return cache[userId];
-  }
+    if (cache[userId]) {
+        return cache[userId];
+    }
 
-  var data = msgpack.unpack(new Buffer(robot.brain.get('impersonateMarkov-' + userId) || ''));
-  data = _.isObject(data) ? data : {};
+    var data = msgpack.unpack(new Buffer(robot.brain.get('impersonateMarkov-' + userId) || ''));
+    data = _.isObject(data) ? data : {};
 
-  var m = new Markov(MIN_WORDS, CASE_SENSITIVE, STRIP_PUNCTUATION);
-  m.import(data);
-  cache[userId] = m;
+    var m = new Markov(MIN_WORDS, CASE_SENSITIVE, STRIP_PUNCTUATION);
+    m.import(data);
+    cache[userId] = m;
 
-  return m;
+    return m;
 }
 
 function start(robot) {
-  var impersonating = false;
-  var lastMessageText;
+    var impersonating = false;
+    var lastMessageText;
 
-  function shouldRespond() {
-    return shouldRespondMode() && impersonating;
-  }
-
-  var cache = {};
-  var store = robotStore.bind(this, robot);
-  var retrieve = robotRetrieve.bind(this, robot, cache);
-
-  robot.brain.setAutoSave(true);
-
-  var hubotMessageRegex = new RegExp('^[@]?(' + robot.name + ')' + (robot.alias ? '|(' + robot.alias + ')' : '') + '[:,]?\\s', 'i');
-
-  robot.respond(/impersonate (\w*)/i, function(msg) {
-    if (shouldRespondMode()) {
-      var username = msg.match[1];
-      var text = msg.message.text;
-
-      var users = robot.brain.usersForFuzzyName(username);
-
-      if (users && users.length > 0) {
-        var user = users[0];
-        impersonating = user.id;
-        msg.send('impersonating ' + user.name);
-
-        var markov = retrieve(impersonating);
-        msg.send(markov.respond(lastMessageText || 'beans'));
-      }
-      else {
-        msg.send("I don't know any " + username + ".");
-      }
+    function shouldRespond() {
+        return shouldRespondMode() && impersonating;
     }
-  });
 
-  robot.respond(/stop impersonating/i, function(msg) {
-    if (shouldRespond()) {
-      var user = robot.brain.userForId(impersonating);
-      impersonating = false;
+    var cache = {};
+    var store = robotStore.bind(this, robot);
+    var retrieve = robotRetrieve.bind(this, robot, cache);
 
-      if (user) {
-        msg.send('stopped impersonating ' + user.name);
-      }
-      else {
-        msg.send('stopped');
-      }
-    }
-    else {
-      msg.send('Wat.');
-    }
-  });
+    robot.brain.setAutoSave(true);
 
-  robot.hear(/.*/, function(msg) {
-    var text = msg.message.text;
-    var markov;
+    var hubotMessageRegex = new RegExp('^[@]?(' + robot.name + ')' + (robot.alias ? '|(' + robot.alias + ')' : '') + '[:,]?\\s', 'i');
 
-    if (text && !hubotMessageRegex.test(text)) {
-      lastMessageText = text;
+    robot.respond(/impersonate (\w*)/i, function(msg) {
+        if (shouldRespondMode()) {
+            var username = msg.match[1];
+            var text = msg.message.text;
 
-      if (shouldTrain()) {
-        var userId = msg.message.user.id;
-        markov = retrieve(userId);
+            var users = robot.brain.usersForFuzzyName(username);
 
-        markov.train(text);
-        store(userId, markov);
-      }
+            if (users && users.length > 0) {
+                var user = users[0];
+                impersonating = user.id;
+                msg.send('impersonating ' + user.name);
 
-      if (shouldRespond() && (Math.floor(Math.random()*(11-1))+1 > Math.floor(Math.random()*(11-1))+1)) {
-          markov = retrieve(impersonating);
-          var markovResponse = markov.respond(text);
-          var baseDelay = RESPONSE_DELAY_PER_WORD*markovResponse.split(" ").length;
-          var totalDelay = Math.random() * (baseDelay*1.5 - baseDelay*0.75) + baseDelay*0.75;
-          setTimeout(function() { msg.send(markovResponse) }, totalDelay);
+                var markov = retrieve(impersonating);
+                msg.send(markov.respond(lastMessageText || 'beans'));
+            } else {
+                msg.send("I don't know any " + username + ".");
+            }
         }
-      }
-    }
-  }
+    });
+
+    robot.respond(/stop impersonating/i, function(msg) {
+        if (shouldRespond()) {
+            var user = robot.brain.userForId(impersonating);
+            impersonating = false;
+
+            if (user) {
+                msg.send('stopped impersonating ' + user.name);
+            } else {
+                msg.send('stopped');
+            }
+        } else {
+            msg.send('Wat.');
+        }
+    });
+
+    robot.hear(/.*/, function(msg) {
+            var text = msg.message.text;
+            var markov;
+
+            if (text && !hubotMessageRegex.test(text)) {
+                lastMessageText = text;
+
+                if (shouldTrain()) {
+                    var userId = msg.message.user.id;
+                    markov = retrieve(userId);
+
+                    markov.train(text);
+                    store(userId, markov);
+                }
+
+                if (shouldRespond() && (Math.floor(Math.random() * (11 - 1)) + 1 > Math.floor(Math.random() * (11 - 1)) + 1)) {
+                    markov = retrieve(impersonating);
+                    var markovResponse = markov.respond(text);
+                    var baseDelay = RESPONSE_DELAY_PER_WORD * markovResponse.split(" ").length;
+                    var totalDelay = Math.random() * (baseDelay * 1.5 - baseDelay * 0.75) + baseDelay * 0.75;
+                    setTimeout(function() {
+                        msg.send(markovResponse);
+                    }, totalDelay);
+                }
+            }
+        }
+    });
 }
 
 module.exports = function(robot) {
-  var loaded = _.once(function() {
-    console.log('starting hubot-impersonate...');
-    start(robot);
-  });
+    var loaded = _.once(function() {
+        console.log('starting hubot-impersonate...');
+        start(robot);
+    });
 
-  if (_.isEmpty(robot.brain.data) || _.isEmpty(robot.brain.data._private)) {
-    robot.brain.once('loaded', loaded);
-    setTimeout(loaded, INIT_TIMEOUT);
-  }
-  else {
-    loaded();
-  }
+    if (_.isEmpty(robot.brain.data) || _.isEmpty(robot.brain.data._private)) {
+        robot.brain.once('loaded', loaded);
+        setTimeout(loaded, INIT_TIMEOUT);
+    } else {
+        loaded();
+    }
 };
